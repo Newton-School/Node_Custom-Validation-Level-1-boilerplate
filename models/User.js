2ddbb11520e699048e9bb95956ab42c8f326405f
adityachandra1 <adityachandra9898@gmail.com>
2023-03-03 17:07:23 +0530

    boilerplate

--- models/User.js
+++ models/User.js
@@ -1,10 +1,6 @@
 /* User Model */
 const mongoose = require('mongoose');
 
-/*
-Add custom validation to the User model
-Validate that the username is at least 4 characters long, doesn't contain spaces, and doesn't contain special characters
-*/
 const userSchema = new mongoose.Schema(
     {
         username: {
